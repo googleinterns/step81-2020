bef0e1ec327078205fca5ce39489049c63bc432e
SydneyCaulfeild <sydney.ec6@gmail.com>
2020-07-14 19:05:22 +0000

    Update to remove conflicts with master.

--- src/main/java/com/google/flourbot/basic/Bot.java
+++ src/main/java/com/google/flourbot/basic/Bot.java
@@ -13,18 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.chat.bot.basic.async;
+package com.google.flourbot.basic;
 
 // [START async-bot]
-
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.chat.v1.HangoutsChat;
 import com.google.api.services.chat.v1.model.Message;
 import com.google.api.services.chat.v1.model.Thread;
+import com.google.api.services.sheets.v4.*;
+import com.google.api.services.sheets.v4.model.*;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
 import org.springframework.boot.SpringApplication;
@@ -36,11 +48,23 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.logging.Logger;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class Bot {
     static final String CHAT_SCOPE = "https://www.googleapis.com/auth/chat.bot";
+    static final String SPREADSHEET_SCOPE = "https://www.googleapis.com/auth/spreadsheets";
+
+    ArrayList<String> SCOPES = new ArrayList<String>();
+    
+
     private static final Logger logger = Logger.getLogger(Bot.class.getName());
 
     public static void main(String[] args) {
@@ -55,6 +79,15 @@ public class Bot {
      */
     @PostMapping("/")
     public void onEvent(@RequestBody JsonNode event) throws IOException, GeneralSecurityException {
+
+        // TODO: Initialize these into ArrayList
+        SCOPES.add(CHAT_SCOPE);
+        SCOPES.add(SPREADSHEET_SCOPE);
+
+
+
+
+      
         String replyText = "";
         switch (event.at("/type").asText()) {
             case "ADDED_TO_SPACE":
@@ -81,6 +114,54 @@ public class Bot {
         }
 
         // [START async-response]
+        
+        
+
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+
+
+        GoogleCredentials credentials = GoogleCredentials.fromStream(
+                Bot.class.getResourceAsStream("/service-acct.json")
+        ).createScoped(SCOPES);
+        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
+        HangoutsChat chatService = new HangoutsChat.Builder(
+                    httpTransport,
+                    jsonFactory,
+                    requestInitializer)
+                .setApplicationName("basic-async-bot-java")
+                .build();
+        
+
+        // NEXT TASK:
+        // Now that we have sheetsService working, create a spreadsheet in my own drive, save the ID into a string
+        // Then, find out how to have the bot request access to edit the sheet (probably going to take the longest time to figure out)
+        // Look into: https://googleapis.dev/java/google-api-client/latest/com/google/api/client/googleapis/auth/oauth2/GoogleAuthorizationCodeFlow.html
+        // And more importantly: https://developers.google.com/api-client-library/java/google-api-java-client/oauth2
+        // once that works, figure out how to write (append) to the sheet
+        // After that, just build out the logic of reading and writing
+
+        
+        // TODO: see if I can use the same GoogleNetHTTPtransport
+
+        // Create sheetsservice
+        Sheets sheetsService = new Sheets.Builder(
+                    GoogleNetHttpTransport.newTrustedTransport(),
+                    jsonFactory,
+                    requestInitializer)
+                .setApplicationName("Google-SheetsSample/0.1")
+                .build();
+
+
+        // make a new sheet and tell the user the ID and URL
+        Spreadsheet requestBody = new Spreadsheet()
+                      .setProperties(new SpreadsheetProperties()
+                        .setTitle("TEST 123"));;
+        Sheets.Spreadsheets.Create request = sheetsService.spreadsheets().create(requestBody);
+        Spreadsheet response = request.execute();
+        replyText = "ID: " + response.getSpreadsheetId() + "URL: " + response.getSpreadsheetUrl();
+
+
         String spaceName = event.at("/space/name").asText();
         Message reply = new Message().setText(replyText);
         // If replying to a message, set thread name to keep conversation together
@@ -90,17 +171,8 @@ public class Bot {
             reply.setThread(thread);
         }
 
-        GoogleCredentials credentials = GoogleCredentials.fromStream(
-                Bot.class.getResourceAsStream("/service-acct.json")
-        ).createScoped(CHAT_SCOPE);
-        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
-        HangoutsChat chatService = new HangoutsChat.Builder(
-                    GoogleNetHttpTransport.newTrustedTransport(),
-                    JacksonFactory.getDefaultInstance(),
-                    requestInitializer)
-                .setApplicationName("basic-async-bot-java")
-                .build();
         chatService.spaces().messages().create(spaceName, reply).execute();
+
         // [END async-response]
     }
 }
