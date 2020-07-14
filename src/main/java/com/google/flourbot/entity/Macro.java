2da6d93f951697a2c65af28e4cd82d685b0b4282
Tony Shen <tsa87@sfu.ca>
2020-07-14 23:47:06 +0000

    make macro class immutable

--- src/main/java/com/google/flourbot/entity/Macro.java
+++ src/main/java/com/google/flourbot/entity/Macro.java
@@ -1,11 +1,11 @@
 package com.google.flourbot.entity;
 
-public class Macro {
-    private String creatorId;
-    private String macroName;
-    private String docId;
-    private Trigger macroTrigger;
-    private Action macroAction;
+public final class Macro {
+    private final String creatorId;
+    private final String macroName;
+    private final String docId;
+    private final Trigger macroTrigger;
+    private final Action macroAction;
     
     public Macro (String creatorId, String macroName, String docId, Object macroTrigger, Object macroAction) {
         this.creatorId = creatorId;
