4c648ae702d840ddc047cb1f43c31d32490f0cb3
SydneyCaulfeild <sydney.ec6@gmail.com>
2020-07-14 18:54:17 +0000

    Modify interface method to accpet a message instead of a macro name and return a document instead of a macro.

--- src/main/java/com/google/flourbot/datastorage/DataStorage.java
+++ src/main/java/com/google/flourbot/datastorage/DataStorage.java
@@ -1,5 +1,5 @@
 package com.google.flourbot.datastorage;
 
 public interface DataStorage {
-    Macro getMacro (String userEmail, String macroName) throws Exception; 
+    QueryDocumentSnapshot getDocument (String userEmail, String message) throws Exception; 
 }
