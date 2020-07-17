d9ee27356a05d546455d8e9c4bf60b6cb1a33c7e
Miasya Bulger <miasya.bulger@mail.mcgill.ca>
2020-07-17 16:32:12 +0000

    segregate drive api into 2 interfaces and 2 classes

--- src/main/java/com/google/flourbot/api/Bot.java
+++ src/main/java/com/google/flourbot/api/Bot.java
@@ -1,25 +1,10 @@
 /**
- * Copyright 2017 Google Inc.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 package com.google.flourbot.api;
 
 // [START async-bot]
 import com.fasterxml.jackson.databind.JsonNode;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -35,8 +20,6 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.chat.v1.HangoutsChat;
 import com.google.api.services.chat.v1.model.Message;
 import com.google.api.services.chat.v1.model.Thread;
-import com.google.api.services.sheets.v4.*;
-import com.google.api.services.sheets.v4.model.*;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
 import org.springframework.boot.SpringApplication;
@@ -51,131 +34,104 @@ import java.util.logging.Logger;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
 @RestController
-private class Bot {
-    static final String CHAT_SCOPE = "https://www.googleapis.com/auth/chat.bot";
-    static final String SPREADSHEET_SCOPE = "https://www.googleapis.com/auth/spreadsheets";
+public class Bot {
+  static final String CHAT_SCOPE = "https://www.googleapis.com/auth/chat.bot";
+  private static final Logger logger = Logger.getLogger(Bot.class.getName());
 
-    ArrayList<String> SCOPES = new ArrayList<String>();
+  public static void main(String[] args) {
+    SpringApplication.run(Bot.class, args);
+  }
+
+  /**
+    * Handles a GET request to the /bot endpoint.
+    *
+    * @param event Event from chat.
+    * @return Message
+    */
+  @PostMapping("/")
+  public void onEvent(@RequestBody JsonNode event) throws IOException, GeneralSecurityException {
+    String replyText = "";
+    switch (event.at("/type").asText()) {
+      case "ADDED_TO_SPACE":
+        String spaceType = event.at("/space/type").asText();
+        if ("ROOM".equals(spaceType)) {
+          String displayName = event.at("/space/displayName").asText();
+          replyText = String.format("Thanks for adding me to %s", displayName);
+        } else {
+          String displayName = event.at("/user/displayName").asText();
+          replyText = String.format("Thanks for adding me to a DM, %s!", displayName);
+        }
+        break;
+      case "MESSAGE":
+        String message = event.at("/message/text").asText();
+
+        // TODO: call execution module
+        String userEmail = event.at("/message/sender/email").asText();
+
+        replyText = String.format("Your message: %s", message);
+        break;
+      case "REMOVED_FROM_SPACE":
+        String name = event.at("/space/name").asText();
+        logger.info(String.format("Bot removed from %s", name));
+        break;
+      default:
+        replyText = "Cannot determine event type";
+        break;
+    }
+
+    // [START async-response]
+    
+    // Set up credentials
+    JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+    NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+    GoogleCredentials credentials = GoogleCredentials.fromStream(
+            Bot.class.getResourceAsStream("/service-acct.json")
+    ).createScoped(CHAT_SCOPE);
+    HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
+
+    // Create chat service
+    HangoutsChat chatService = new HangoutsChat.Builder(
+            httpTransport,
+            jsonFactory,
+            requestInitializer)
+          .setApplicationName("bot-chat")
+          .build();
+
     
 
-    private static final Logger logger = Logger.getLogger(Bot.class.getName());
+    // Make it work without execution module for now
+    ArrayList<String> values = new ArrayList<String>();
+    values.add("Today");
+    values.add(event.at("/message/sender/displayName").asText());
+    values.add(event.at("/message/text").asText());
 
-    public static void main(String[] args) {
-        SpringApplication.run(Bot.class, args);
+    String documentId = "1199pOyaWIa67VpvJsKQ7NvxebyyznhAquxYYV9moZkc";
+    DriveClient cdc = new DriveClient();
+    CloudSheet cs = cdc.getCloudSheet(documentId);
+    cs.appendRow(values);
+
+    // replyText = "ID: " + response.getSpreadsheetId();
+
+    // Generate and send request to post in chat room
+    String spaceName = event.at("/space/name").asText();
+    Message reply = new Message().setText(replyText);
+    // If replying to a message, set thread name to keep conversation together
+    if (event.has("message")) {
+      String threadName = event.at("/message/thread/name").asText();
+      Thread thread = new Thread().setName(threadName);
+      reply.setThread(thread);
     }
 
-    /**
-     * Handles a GET request to the /bot endpoint.
-     *
-     * @param event Event from chat.
-     * @return Message
-     */
-    @PostMapping("/")
-    public void onEvent(@RequestBody JsonNode event) throws IOException, GeneralSecurityException {
-
-        // TODO: Initialize these into ArrayList
-        SCOPES.add(CHAT_SCOPE);
-        SCOPES.add(SPREADSHEET_SCOPE);
-
-
-
-
-      
-        String replyText = "";
-        switch (event.at("/type").asText()) {
-            case "ADDED_TO_SPACE":
-                String spaceType = event.at("/space/type").asText();
-                if ("ROOM".equals(spaceType)) {
-                    String displayName = event.at("/space/displayName").asText();
-                    replyText = String.format("Thanks for adding me to %s", displayName);
-                } else {
-                    String displayName = event.at("/user/displayName").asText();
-                    replyText = String.format("Thanks for adding me to a DM, %s!", displayName);
-                }
-                break;
-            case "MESSAGE":
-                String message = event.at("/message/text").asText();
-                replyText = String.format("Your message: %s", message);
-                break;
-            case "REMOVED_FROM_SPACE":
-                String name = event.at("/space/name").asText();
-                logger.info(String.format("Bot removed from %s", name));
-                break;
-            default:
-                replyText = "Cannot determine event type";
-                break;
-        }
-
-        // [START async-response]
-        
-        
-
-        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-
-
-        GoogleCredentials credentials = GoogleCredentials.fromStream(
-                Bot.class.getResourceAsStream("/service-acct.json")
-        ).createScoped(SCOPES);
-        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
-        HangoutsChat chatService = new HangoutsChat.Builder(
-                    httpTransport,
-                    jsonFactory,
-                    requestInitializer)
-                .setApplicationName("basic-async-bot-java")
-                .build();
-        
-
-        // NEXT TASK:
-        // Now that we have sheetsService working, create a spreadsheet in my own drive, save the ID into a string
-        // Then, find out how to have the bot request access to edit the sheet (probably going to take the longest time to figure out)
-        // Look into: https://googleapis.dev/java/google-api-client/latest/com/google/api/client/googleapis/auth/oauth2/GoogleAuthorizationCodeFlow.html
-        // And more importantly: https://developers.google.com/api-client-library/java/google-api-java-client/oauth2
-        // once that works, figure out how to write (append) to the sheet
-        // After that, just build out the logic of reading and writing
-
-        
-        // TODO: see if I can use the same GoogleNetHTTPtransport
-
-        // Create sheetsservice
-        Sheets sheetsService = new Sheets.Builder(
-                    GoogleNetHttpTransport.newTrustedTransport(),
-                    jsonFactory,
-                    requestInitializer)
-                .setApplicationName("Google-SheetsSample/0.1")
-                .build();
-
-
-        // make a new sheet and tell the user the ID and URL
-        Spreadsheet requestBody = new Spreadsheet()
-                      .setProperties(new SpreadsheetProperties()
-                        .setTitle("TEST 123"));;
-        Sheets.Spreadsheets.Create request = sheetsService.spreadsheets().create(requestBody);
-        Spreadsheet response = request.execute();
-        replyText = "ID: " + response.getSpreadsheetId() + "URL: " + response.getSpreadsheetUrl();
-
-
-        String spaceName = event.at("/space/name").asText();
-        Message reply = new Message().setText(replyText);
-        // If replying to a message, set thread name to keep conversation together
-        if (event.has("message")) {
-            String threadName = event.at("/message/thread/name").asText();
-            Thread thread = new Thread().setName(threadName);
-            reply.setThread(thread);
-        }
-
-        chatService.spaces().messages().create(spaceName, reply).execute();
-
-        // [END async-response]
-    }
+    chatService.spaces().messages().create(spaceName, reply).execute();
+    // [END async-response]
+  }
 }
 
 // [END async-bot]
-
