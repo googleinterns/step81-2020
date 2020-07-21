0cec57eba47012d38916a3ed2e1ebfffcaa99260
SydneyCaulfeild <sydney.ec6@gmail.com>
2020-07-21 14:18:43 +0000

    Specify import statements by removing *

--- src/main/java/com/google/flourbot/entity/EntityModuleImplementation.java
+++ src/main/java/com/google/flourbot/entity/EntityModuleImplementation.java
@@ -13,7 +13,7 @@ import java.util.Collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.*;
+import java.util.stream.Stream;
 
 public class EntityModuleImplementation implements EntityModule {
 
