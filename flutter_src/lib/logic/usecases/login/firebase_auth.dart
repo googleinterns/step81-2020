8db25b4944bea1f1d2d9d2a33e9a1636716eabf1
tsa87 <tsa87@sfu.ca>
2020-07-22 11:24:48 -0700

    Convert to unchecked exception

--- flutter_src/lib/logic/usecases/login/firebase_auth.dart
+++ flutter_src/lib/logic/usecases/login/firebase_auth.dart
@@ -34,7 +34,6 @@ class FirebaseAuthService {
   }
 
   Future<User> signInWithGoogle() async {
-    try {
       final googleUser = await _googleSignIn.signIn();
       final googleAuth = await googleUser.authentication;
       final credential = GoogleAuthProvider.getCredential(
@@ -43,9 +42,6 @@ class FirebaseAuthService {
       );
       final authResult = await _firebaseAuth.signInWithCredential(credential);
       return _userFromFirebase(authResult.user);
-    } catch(e) {
-      return null;
-    }
   }
 
   Future<void> signOut() async {
