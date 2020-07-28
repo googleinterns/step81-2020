69db83c3d01916b1a1e15a290cee3868f33f5965
Miasya Bulger <miasya.bulger@mail.mcgill.ca>
2020-07-28 16:42:56 +0000

    make regex pattern a constant

--- src/main/java/com/google/flourbot/entity/action/sheet/SheetAction.java
+++ src/main/java/com/google/flourbot/entity/action/sheet/SheetAction.java
@@ -7,7 +7,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public abstract class SheetAction extends Action {
-
+  
+  private static final String REGEX_PATTERN = "(d\\/[a-zA-Z0-9-_]+)";
   protected final String documentUrl;
 
   public SheetAction(String documentUrl) {
@@ -22,7 +23,7 @@ public abstract class SheetAction extends Action {
 
   public String getDocumentId() {
     String documentId = "";
-    Pattern pattern = Pattern.compile("(d\\/[a-zA-Z0-9-_]+)");
+    Pattern pattern = Pattern.compile(REGEX_PATTERN);
     Matcher matcher = pattern.matcher(documentUrl);
     // Take the first occurance
     if (matcher.find()) {
