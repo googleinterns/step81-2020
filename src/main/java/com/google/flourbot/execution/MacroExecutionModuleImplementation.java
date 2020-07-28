7dde4d5624897e36a7dfaa95010dd37f254894b1
SydneyCaulfeild <sydney.ec6@gmail.com>
2020-07-28 19:23:54 +0000

    Add getReplyText and removeMacro functions.

--- src/main/java/com/google/flourbot/execution/MacroExecutionModuleImplementation.java
+++ src/main/java/com/google/flourbot/execution/MacroExecutionModuleImplementation.java
@@ -18,10 +18,13 @@ import java.security.GeneralSecurityException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import java.util.Optional;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Optional;
 
 // The Logic class of the server
 public class MacroExecutionModuleImplementation implements MacroExecutionModule {
@@ -29,11 +32,7 @@ public class MacroExecutionModuleImplementation implements MacroExecutionModule 
   private final EntityModule entityModule;
   private final CloudDocClient cloudDocClient;
   private static HashMap<String, String> threadMacroMap = new HashMap<String, String>();
-
-  private MacroExecutionModuleImplementation(EntityModule entityModule, CloudDocClient cloudDocClient) {
-    this.entityModule = entityModule;
-    this.cloudDocClient = cloudDocClient;
-  }
+  private static Map<String, Map<String, String>> roomToMacro = new HashMap<String, Map<String, String>>();
 
   public static MacroExecutionModuleImplementation initializeServer() {
     DataStorage dataStorage = new FirebaseDataStorage();
@@ -60,7 +59,68 @@ public class MacroExecutionModuleImplementation implements MacroExecutionModule 
     return macroName;
   }
 
-  public String execute(String userEmail, String macroCreatorEmail, String message, String threadId, String macroName) throws IOException, GeneralSecurityException {
+  public String getReplyText(String[] words, String message, String threadId, String roomId, String messageSenderEmail, String helpMessage) throws IOException, GeneralSecurityException{
+        String macroName;
+
+        // Check that a message was written.
+        if (words.length <= 1) {
+            return "You must type a message when you message me. Please type \"@MacroBot /help\" for more instructions.";
+        }    
+
+        // Reply to the help message.
+        if (words[1].equalsIgnoreCase("/help")) {  
+            return helpMessage; 
+        }
+
+        // Get the macro name from the message. 
+        if (words[1].equalsIgnoreCase("/share")) {
+            String modifiedMessage = removeShareFromMessage(words);
+            macroName = getMacroName(modifiedMessage, threadId);
+        }  
+        else {
+            macroName = getMacroName(message, threadId);
+        }
+
+        // If the macro has already been shared to this room, execute the macro. 
+        if (roomToMacro.containsKey(roomId)) {
+            Map<String, String> macroToCreator = roomToMacro.get(roomId);
+            String macroCreatorEmail = macroToCreator.get(macroName);
+            return execute(messageSenderEmail, macroCreatorEmail, message, threadId, macroName);
+        }
+        // If the macro has not been shared to the room, query the database for the macro.
+        else {
+            Optional<Macro> optionalMacro = entityModule.getMacro(messageSenderEmail, macroName);
+            if (!optionalMacro.isPresent()) {
+                return "You do not own/have access to this macro.";
+            }
+            else {
+                // Share the macro if the creator writes "/share".
+                if (words[1].equalsIgnoreCase("/share")) {
+                    String macroCreatorEmail = messageSenderEmail;
+                    Map<String, String> macroToCreator = new HashMap<String, String> ();
+                    macroToCreator.put(macroName, macroCreatorEmail);
+
+                    roomToMacro.put(roomId, macroToCreator);
+                    return String.format("The %s macro belonging to %s has been shared to this room. All users in this room can use this macro.", macroName, macroCreatorEmail);
+                }
+                // Execute the macro if the creator just wants to use it instead of share it.
+                else {
+                    return execute(messageSenderEmail, messageSenderEmail, message, threadId, macroName);
+                }
+            }
+        }
+  }
+
+  public void removeMacro(String roomId) {
+      roomToMacro.remove(roomId);
+  }
+
+  private MacroExecutionModuleImplementation(EntityModule entityModule, CloudDocClient cloudDocClient) {
+    this.entityModule = entityModule;
+    this.cloudDocClient = cloudDocClient;
+  }
+
+  private String execute(String userEmail, String macroCreatorEmail, String message, String threadId, String macroName) throws IOException, GeneralSecurityException {
 
     Optional<Macro> optionalMacro = entityModule.getMacro(macroCreatorEmail, macroName);
     if (!optionalMacro.isPresent()) {
@@ -150,5 +210,11 @@ public class MacroExecutionModuleImplementation implements MacroExecutionModule 
     LocalDateTime myDateObj = LocalDateTime.now();
     DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern(pattern);
     return myDateObj.format(myFormatObj);
+  }
+
+  private String removeShareFromMessage (String[] words) {
+      List<String> wordsWithoutShare = new LinkedList(Arrays.asList(words));
+      wordsWithoutShare.remove("/share");
+      return String.join(" ", wordsWithoutShare);
   }
 }
