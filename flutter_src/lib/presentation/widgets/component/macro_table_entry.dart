c713dd52f4bda46ba4bff76f6ed9ba4bbd261840
tsa87 <tsa87@sfu.ca>
2020-07-29 16:23:23 -0700

    Clean up MacroTableEntry

--- flutter_src/lib/presentation/widgets/component/macro_table_entry.dart
+++ flutter_src/lib/presentation/widgets/component/macro_table_entry.dart
@@ -1,19 +1,27 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_bloc/flutter_form_bloc.dart';
 import 'package:macrobaseapp/logic/api/firestore_db.dart';
 import 'package:macrobaseapp/logic/state/macro_state/macro_notifier.dart';
+import 'package:macrobaseapp/logic/state/team_state/team_notifier.dart';
 import 'package:macrobaseapp/model/entities/macro.dart';
 import 'package:macrobaseapp/presentation/pages/macro_detail.dart';
+import 'package:macrobaseapp/presentation/widgets/form/drop_down_menu.dart';
 import 'package:provider/provider.dart';
 
-class MacroTableEntry extends StatelessWidget {
-  const MacroTableEntry({
-    Key key,
-    @required this.macro, this.onDelete, this.onClick,
-  }) : super(key: key);
-
+class MacroTableEntry extends StatefulWidget {
   final Macro macro;
-  final Function onDelete;
-  final Function onClick;
+
+  const MacroTableEntry({Key key, this.macro})
+      : super(key: key);
+  @override
+  _MacroTableEntryState createState() =>
+      _MacroTableEntryState(macro);
+}
+
+class _MacroTableEntryState extends State<MacroTableEntry> {
+  final Macro macro;
+  _MacroTableEntryState(this.macro);
 
   @override
   Widget build(BuildContext context) {
@@ -22,25 +30,23 @@ class MacroTableEntry extends StatelessWidget {
         leading: FlutterLogo(size: 72.0),
         title: Text(macro.macroName),
         subtitle: Text(macro.description),
-        trailing: IconButton(
-          icon: Icon(Icons.delete),
-          onPressed: () {
-            FirestoreService db = FirestoreService();
-            db.removeObject('macros', macro.macroId);
-//            macroNotifier.deleteMacro(macroNotifier.macroList[index]);
-          },
-        ),
+        trailing: IconButton(icon: Icon(Icons.delete), onPressed: () {
+          FirestoreService db = FirestoreService();
+          db.removeObject('macros', macro.macroId);
+          MacroNotifier().macroList.remove(macro);
+        }),
         onTap: () {
-//          macroNotifier.currentMacro = macroNotifier.macroList[index];
-//          Navigator.of(context).push(
-//            MaterialPageRoute(
-//              builder: (BuildContext context) {
-//                return MacroDetail();
-//              },
-//            ),
-//          );
+          MacroNotifier().currentMacro = macro;
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (BuildContext context) {
+                return MacroDetail();
+              },
+            ),
+          );
         },
       ),
     );
   }
 }
+
