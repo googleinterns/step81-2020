37833cb3edff60646ce373fbb1838b67767ce576
tsa87 <tsa87@sfu.ca>
2020-07-29 17:26:50 -0700

    Specify onDelete function in MacroTablePage

--- flutter_src/lib/presentation/pages/macro_table_page.dart
+++ flutter_src/lib/presentation/pages/macro_table_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:macrobaseapp/logic/api/firestore_db.dart';
 import 'package:macrobaseapp/logic/state/team_state/team_notifier.dart';
+import 'package:macrobaseapp/model/adapters/team_model.dart';
 import 'package:macrobaseapp/model/entities/macro.dart';
 import 'package:macrobaseapp/model/entities/team.dart';
+import 'package:macrobaseapp/presentation/pages/team_detail.dart';
 import 'package:macrobaseapp/presentation/widgets/misc/no_macro_illustration.dart';
 import 'package:macrobaseapp/presentation/widgets/component/macro_table_entry.dart';
 import 'package:macrobaseapp/logic/state/macro_state/macro_notifier.dart';
@@ -21,9 +23,9 @@ class _MacroTableState extends State<MacroTable> {
   @override
   Widget build(BuildContext context) {
     MacroNotifier macroNotifier =
-        Provider.of<MacroNotifier>(context, listen: true);
+    Provider.of<MacroNotifier>(context, listen: true);
     TeamNotifier teamNotifier =
-        Provider.of<TeamNotifier>(context, listen: true);
+    Provider.of<TeamNotifier>(context, listen: true);
 
     final user = Provider.of<User>(context);
 
@@ -41,7 +43,6 @@ class _MacroTableState extends State<MacroTable> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.start,
             children:
-            teamNotifier.teamList.length == 0 ? Center(child: Text("No Team has been built yet")) :
             teamNotifier.teamList
                 .map((team) => _buildDragTarget(team))
                 .toList(),
@@ -56,7 +57,6 @@ class _MacroTableState extends State<MacroTable> {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.end,
               children:
-              macroNotifier.macroList.length == 0 ? NoMacroIllustration() :
               macroNotifier.macroList.map((macro) {
                 return Draggable<Macro>(
                   data: macro,
@@ -66,6 +66,10 @@ class _MacroTableState extends State<MacroTable> {
                             maxWidth: MediaQuery.of(context).size.width),
                         child: MacroTableEntry(
                           macro: macro,
+                          onDelete: () {
+                            db.removeObject('macros', macro.macroId);
+                            MacroNotifier().macroList.remove(macro);
+                          },
                         )),
                   ),
                   feedback: Material(
@@ -74,6 +78,10 @@ class _MacroTableState extends State<MacroTable> {
                             maxWidth: MediaQuery.of(context).size.width),
                         child: MacroTableEntry(
                           macro: macro,
+                          onDelete: () {
+                            db.removeObject('macros', macro.macroId);
+                            MacroNotifier().macroList.remove(macro);
+                          },
                         )),
                   ),
                   childWhenDragging: Container(),
@@ -85,14 +93,31 @@ class _MacroTableState extends State<MacroTable> {
   }
 
   Widget _buildDragTarget(Team team) {
+    TeamNotifier teamNotifier =
+    Provider.of<TeamNotifier>(context, listen: true );
+
     return DragTarget<Macro>(
       builder: (BuildContext context, List<Macro> incoming, List rejected) {
         return Card(
           child: ListTile(
-            leading: FlutterLogo(size: 56.0),
-            title: Text(team.name),
-            subtitle: Text(team.description),
-            trailing: Icon(Icons.more_vert),
+              leading: FlutterLogo(size: 56.0),
+              title: Text(team.name),
+              subtitle: Text(team.description),
+              trailing: IconButton(icon: Icon(Icons.delete), onPressed: () {
+                FirestoreService db = FirestoreService();
+                db.removeObject('teams', team.teamId);
+                teamNotifier.teamList.remove(team);
+              }),
+              onTap: () {
+                teamNotifier.currentTeam = team;
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return TeamDetail();
+                    },
+                  ),
+                );
+              }
           ),
         );
       },
@@ -100,6 +125,11 @@ class _MacroTableState extends State<MacroTable> {
       onAccept: (data) {
         Scaffold.of(context).showSnackBar(SnackBar(
             content: Text(data.macroName + " added to " + team.name + "!")));
+        team.macros.add(data);
+
+        FirestoreService db = FirestoreService();
+        db.removeObject('teams', team.teamId);
+        db.uploadObject('teams', (team as TeamModel).toJson());
       },
       onLeave: (data) {},
     );
