33ce900f53442b6a720071d1153c91d33927f50b
tsa87 <tsa87@sfu.ca>
2020-07-29 19:43:19 -0700

    Minor refactor

--- flutter_src/lib/logic/api/firestore_db.dart
+++ flutter_src/lib/logic/api/firestore_db.dart
@@ -9,38 +9,43 @@ import 'package:macrobaseapp/model/entities/team.dart';
 class FirestoreService {
   final Firestore db;
 
-  FirestoreService({FirestoreService db})
-    : db = db ?? Firestore.instance;
+  FirestoreService({FirestoreService db}) : db = db ?? Firestore.instance;
 
-  void getEntries(TeamNotifier teamNotifier, MacroNotifier macroNotifier, String creatorId) {
+  void getEntries(TeamNotifier teamNotifier, MacroNotifier macroNotifier,
+      String creatorId) {
     _getMacros(macroNotifier, creatorId);
     _getTeams(teamNotifier, creatorId);
   }
 
   void _getMacros(MacroNotifier macroNotifier, String creatorId) async {
-    QuerySnapshot snapshot = await db.collection('macros').where(
-        "creatorId", isEqualTo: creatorId).getDocuments();
+    QuerySnapshot snapshot = await db
+        .collection('macros')
+        .where("creatorId", isEqualTo: creatorId)
+        .getDocuments();
 
     List<Macro> _macroList = [];
 
     snapshot.documents.forEach((element) {
-        Macro macro = MacroModel.fromJson(element.data);
-        macro.macroId = element.documentID;
-        _macroList.add(macro);
+      Macro macro = MacroModel.fromJson(element.data);
+      macro.macroId = element.documentID;
+      _macroList.add(macro);
     });
 
     macroNotifier.macroList = _macroList;
   }
 
   void _getTeams(TeamNotifier teamNotifier, String creatorId) async {
-    QuerySnapshot snapshot = await db.collection('teams').where("creatorId", isEqualTo: creatorId).getDocuments();
+    QuerySnapshot snapshot = await db
+        .collection('teams')
+        .where("creatorId", isEqualTo: creatorId)
+        .getDocuments();
 
     List<Team> _teamList = [];
 
     snapshot.documents.forEach((element) {
-        Team team = TeamModel.fromJson(element.data);
-        team.teamId = element.documentID;
-        _teamList.add(team);
+      Team team = TeamModel.fromJson(element.data);
+      team.teamId = element.documentID;
+      _teamList.add(team);
     });
 
     teamNotifier.teamList = _teamList;
@@ -50,15 +55,13 @@ class FirestoreService {
     return db.collection(collectionName).document(macroId).delete();
   }
 
-
   void uploadObject(String collectionName, Map<String, dynamic> json) {
-    db.collection(collectionName).document()
-        .setData(json);
+    db.collection(collectionName).document().setData(json);
   }
 
-
   Future<List<Macro>> queryMacro(String macroName) async {
-    QuerySnapshot snapshot = await db.collection('macros')
+    QuerySnapshot snapshot = await db
+        .collection('macros')
         .where("macroName", isEqualTo: macroName)
         .getDocuments();
 
@@ -72,6 +75,3 @@ class FirestoreService {
     return _macroList;
   }
 }
-
-
-
