86102978e5f2adcea8e53aedb688d5282811e2d5
Miasya Bulger <miasya.bulger@mail.mcgill.ca>
2020-07-31 18:55:55 +0000

    add tests for reading blocks from a range

--- src/test/java/com/google/flourbot/api/DriveCloudSheetTest.java
+++ src/test/java/com/google/flourbot/api/DriveCloudSheetTest.java
@@ -23,8 +23,8 @@ public final class DriveCloudSheetTest {
   private static final String SPREADSHEET_ID = "1ruk-1_izE0iiOWqf5mgyHkOcW2dCNI09uy28kuceJtQ";
   private static final String SHEET_NAME = "Sheet1";
 
-  private static final String RANGE_SINGLE = "B1";
-  private static final String RANGE_SINGLE_EMPTY = "F1";
+  private static final String RANGE_SINGLE = "B1:B1";
+  private static final String RANGE_SINGLE_EMPTY = "F1:F1";
   private static final String RANGE_BLOCK = "A1:C3";
   private static final String RANGE_BLOCK_EMPTY = "G2:H4";
   private static final String RANGE_ROW = "2";
@@ -49,7 +49,7 @@ public final class DriveCloudSheetTest {
   @Test
   public void testReadRangeSingle() {
     List<List<String>> response = cloudSheet.readRange(
-        String.format("%s!%s:%s", SHEET_NAME, RANGE_SINGLE, RANGE_SINGLE));
+        String.format("%s!%s", SHEET_NAME, RANGE_SINGLE));
 
     String[] array = {"B1"};
     List<List<String>> expected = Arrays.asList(
@@ -62,9 +62,31 @@ public final class DriveCloudSheetTest {
   @Test
   public void testReadRangeSingleEmpty() {
     List<List<String>> response = cloudSheet.readRange(
-        String.format("%s!%s:%s", SHEET_NAME, RANGE_SINGLE_EMPTY, RANGE_SINGLE_EMPTY));
+        String.format("%s!%s", SHEET_NAME, RANGE_SINGLE_EMPTY));
     Assert.assertEquals(null, response);
   }
 
-  
+  @Test
+  public void testReadRangeBlock() {
+    List<List<String>> response = cloudSheet.readRange(
+        String.format("%s!%s", SHEET_NAME, RANGE_BLOCK));
+
+    String[] array1 = {"A1", "B1", "C1"};
+    String[] array2 = {"A2", "B2", "C2"};
+    String[] array3 = {"A3", "B3", "C3"};
+    List<List<String>> expected = Arrays.asList(
+      Arrays.asList(array1),
+      Arrays.asList(array2),
+      Arrays.asList(array3)
+    );
+
+    Assert.assertEquals(expected, response);
+  }
+
+  @Test
+  public void testReadRangeBlockEmpty() {
+    List<List<String>> response = cloudSheet.readRange(
+        String.format("%s!%s", SHEET_NAME, RANGE_BLOCK_EMPTY));
+    Assert.assertEquals(null, response);
+  }
 }
