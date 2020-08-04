c3999dbdede944d7d7ca47270a85625668ae9839
tsa87 <tsa87@sfu.ca>
2020-08-04 16:11:51 -0700

    Add new EntityModule tests about malformed Map response

--- src/test/java/com/google/flourbot/entity/EntityModuleImplementationTest.java
+++ src/test/java/com/google/flourbot/entity/EntityModuleImplementationTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,6 +37,7 @@ class EntityModuleImplementationTest {
     entityModuleImplementation  = new EntityModuleImplementation(dataStorage);
   }
 
+  // Test getMacro under normal scenario - Database returns a well formed Macro Map object
   @Test
   void getMacro() throws IOException {
     String mockJson = loadTestJson("mock_macro.json");
@@ -50,10 +52,39 @@ class EntityModuleImplementationTest {
         new SheetAppendRowAction(
             "https://docs.google.com/spreadsheets/d/YOUR_SHEET_ID_HERE/edit#gid=0", new SheetEntryType[0])
     );
-
     Macro actualMacro = entityModuleImplementation.getMacro("tonyshen@google.com", "Daily").get();
 
     assertEquals(actualMacro, expectedMacro);
+  }
+
+  // Test scenario where Map received is null.
+  @Test
+  void getMacroEmpty() {
+    dataStorage.setMap(null);
+    Optional<Macro> optionalMacro = entityModuleImplementation.getMacro("", "");
+    assertTrue(optionalMacro.isEmpty());
+  }
+
+  @Test
+  void getMacroEmptyAction() throws IOException {
+    String mockJson = loadTestJson("mock_macro_no_action.json");
+    Map<String, Object> mockMap =
+        new ObjectMapper().readValue(mockJson, Map.class);
+    dataStorage.setMap(mockMap);
+
+    Optional<Macro> optionalMacro = entityModuleImplementation.getMacro("tonyshen@google.com", "Daily");
+    assertTrue(optionalMacro.isEmpty());
+  }
+
+  @Test
+  void getMacroEmptyTrigger() throws IOException {
+    String mockJson = loadTestJson("mock_macro_no_trigger.json");
+    Map<String, Object> mockMap =
+        new ObjectMapper().readValue(mockJson, Map.class);
+    dataStorage.setMap(mockMap);
+
+    Optional<Macro> optionalMacro = entityModuleImplementation.getMacro("tonyshen@google.com", "Daily");
+    assertTrue(optionalMacro.isEmpty());
   }
 
   @org.junit.jupiter.api.AfterEach
