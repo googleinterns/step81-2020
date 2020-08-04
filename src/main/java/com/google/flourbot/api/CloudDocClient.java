e33afdacf45cdff0abb1630262a8e6fac871be82
Miasya Bulger <miasya.bulger@mail.mcgill.ca>
2020-08-04 18:53:41 +0000

    change exception handling to try catch

--- src/main/java/com/google/flourbot/api/CloudDocClient.java
+++ src/main/java/com/google/flourbot/api/CloudDocClient.java
@@ -8,5 +8,5 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 public interface CloudDocClient {
-  public CloudSheet getCloudSheet(String documentId) throws IOException, GeneralSecurityException;
+  public CloudSheet getCloudSheet(String documentId);
 }
